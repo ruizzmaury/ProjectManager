8cf2b0690fe49cd3825fb2f281136ff1d90d49a7
Maury Ruiz <mauriruizplaza@gmail.com>
2020-09-24 22:14:51 +0200

    inputsFest

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Greeting } from "./components/Greeting";
-
-// const isNewToReact = false;
-// const name: string = "Maury";
-
-// const greeting = <div>Hello React</div>;
-
-// function sayGreeting() {
-//   if (isNewToReact) {
-//     // ... or returned from functions, etc.
-//     return greeting; // displays: Hello React
-//   } else {
-//     return (
-//       <div>
-//         <h1>KFUE</h1>
-//         <div>Hi again {name}, welcome to React</div>
-//         <input name="" />
-//         <button className="submit-button">Submit</button>
-//       </div>
-//     );
-//   }
-// }
-
-
+import { ProjectInput } from "./components/ProjectInput";
+import "bootswatch/dist/lumen/bootstrap.min.css";
 
 function App() {
-  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {<Greeting name={"Maury"}/>}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PROJECT MANAGER</h1>
       </header>
+
+      <body className="App-body">
+        {" "}
+        <ProjectInput inputType="Title"></ProjectInput>
+        <ProjectInput inputType="Description"></ProjectInput>
+        <button>ADD PROJECT</button>
+      </body>
     </div>
   );
 }
---
+++ src/components/ProjectsToDo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class ProjectsToDo extends React.Component {
+    render() {
+        return (<div></div>);
+    }
+}
